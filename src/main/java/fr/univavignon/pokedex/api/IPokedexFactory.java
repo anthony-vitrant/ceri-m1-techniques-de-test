7658f1b26ed7f8b147e159beb98c9f61b1169302
anthony-vitrant <anthony.vitrant@alumni.univ-avignon.fr>
2023-03-28 16:08:39 +0200

    tp3

--- src/main/java/fr/univavignon/pokedex/api/IPokedexFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokedexFactory.java
@@ -1,11 +1,14 @@
 package fr.univavignon.pokedex.api;
 
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Factory interface for class that aims to create IPokedex instance.
  * 
  * @author fv
  */
-public interface IPokedexFactory {
+public interface IPokedexFactory extends IPokedex {
 
 	/**
 	 * Creates a new pokedex instance using the given 
@@ -16,5 +19,25 @@ public interface IPokedexFactory {
 	 * @return Created pokedex instance.
 	 */
 	IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory);
-	
+
+	@Override
+	int size();
+
+	@Override
+	int addPokemon(Pokemon pokemon);
+
+	@Override
+	Pokemon getPokemon(int id) throws PokedexException;
+
+	@Override
+	List<Pokemon> getPokemons();
+
+	@Override
+	List<Pokemon> getPokemons(Comparator<Pokemon> order);
+
+	@Override
+	Pokemon createPokemon(int index, int cp, int hp, int dust, int candy);
+
+	@Override
+	PokemonMetadata getPokemonMetadata(int index) throws PokedexException;
 }
